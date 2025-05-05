02843a6c23e48c055938fc359b34d57c152a60a4
Christopher Williams <williamstophef@gmail.com>
2025-05-05 12:51:54 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+ bool IsNumPalinDrome(int x)
+{
+    if (x < 0)
+    {
+        return false;
+    }
+
+        int original = x;
+    int reversed = 0;
+
+    while (x > 0)
+    {
+        int digit = x % 10;
+        reversed = reversed * 10 + digit;
+        x /= 10;
+    }
+    return original == reversed;
+}
+
+Console.WriteLine(IsNumPalinDrome(121));
+Console.WriteLine(IsNumPalinDrome(123451));
+Console.WriteLine(IsNumPalinDrome(123454321));
+Console.WriteLine(IsNumPalinDrome(-121));
